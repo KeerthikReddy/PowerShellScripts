1c6d58360f22f012e56b1a4383d131d35cca2818
KeerthikReddy <keerthik.reddy15@gmail.com>
2018-06-26 17:59:17 +0530

    Add files via upload
    Added PS script, which would open IE browser, enter some values to the text field and clicks submit button. We could use this script to automate browser activity.

---
+++ OpenBrowserAndClick.ps1
@@ -0,0 +1,19 @@
+﻿$IEInstance = New-Object -com internetexplorer.application
+$IEInstance.visible = $true;
+$IEInstance.navigate("https://ess.excelityglobal.com/index.jsp")
+while($IEInstance.busy) {sleep 1}
+
+$InstanceDocument = $IEInstance.Document
+
+
+
+
+$PIN = $InstanceDocument.IHTMLDocument3_getElementsByTagName('input') | Where-Object {$_.name -eq 'p1'}
+$PIN.value = '0094848192'
+$password = $InstanceDocument.IHTMLDocument3_getElementsByTagName('input') | Where-Object {$_.name -eq 'p2'}
+$password.value = 'test'
+
+sleep 3
+
+$SignIn = $InstanceDocument.IHTMLDocument3_getElementsByTagName('input') | Where-Object {$_.name -eq 'logon'}
+$SignIn.click()
